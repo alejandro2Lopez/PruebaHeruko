bfb43b5381819c5d8c6dd9d11a31895947136603
Juan-Diego07 <juandiegoprendas@gmail.com>
2022-07-19 13:32:00 -0600

    JPR/B96105 user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
   
   def index
-    @users = User.all
-    #@q = User.ransack(params[:q])
-    #@users = @q.result.includes(:name, :mail, :address, :role)
+    #@users = User.all
+    @q = User.ransack(params[:q])
+    @users = @q.result
   end
 
   def show; end
